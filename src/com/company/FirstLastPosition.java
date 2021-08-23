9ef80ee8e8804a67b09db5697d9c13cd037ff519
Samadrita Shaw <88196245+Samadrita-Shaw@users.noreply.github.com>
2021-08-23 21:45:49 +0530

    DSAwithKunal Assignment Solution

---
+++ src/com/company/FirstLastPosition.java
@@ -0,0 +1,40 @@
+// Question: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+public class FirstLastPosition
+{
+      public int[] searchRange(int[] nums, int target)
+    {
+           int[] ans = {-1,-1};
+        // Check first occurrence of target first
+        ans[0]= search(nums, target, true);
+        if(ans[0]!=-1)
+        {
+            ans[1]= search(nums, target, false);
+        }
+        return ans;
+    }
+    public static int search(int[] nums, int target, boolean findStartIndex)  //returning index
+    {
+        int start = 0;
+        int end = nums.length-1;
+        int ans= -1;
+        while(start<=end)
+        {
+            int mid = start+(end-start)/2;
+            if(nums[mid]>target)
+                end= mid-1;
+            else if(nums[mid]<target)
+                start= mid+1;
+            else      
+            {
+                ans = mid;       //possible answer found
+                if(findStartIndex)
+                {
+                    end= mid-1;
+                }
+                else
+                    start= mid+1;
+            }
+        }
+        return ans;
+    }
+}
