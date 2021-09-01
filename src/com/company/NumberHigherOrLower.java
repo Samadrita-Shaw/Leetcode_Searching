19ae473c2a00aa71fa3c8128324e39f08c14d696
Samadrita Shaw <88196245+Samadrita-Shaw@users.noreply.github.com>
2021-09-01 08:55:23 +0530

    DSAWithKunal Searching Assignment

---
+++ src/com/company/NumberHigherOrLower.java
@@ -0,0 +1,21 @@
+// Question: https://leetcode.com/problems/guess-number-higher-or-lower/
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) 
+    {
+        int start = 1, end = n;
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            if(guess(mid)==0)   // if the guessed number == picked number
+                return mid;
+            
+            else if(guess(mid)==1)  // if the guessed number is less than the picked number
+                start=mid+1;        // the picked number lies in the second half
+            
+            else if(guess(mid)==-1)  //if the guessed number is greater than the picked number
+                end=mid-1;           //// the picked number lies in the first half
+        }
+        return -1;
+    }
+}
